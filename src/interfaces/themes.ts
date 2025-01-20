60d8c972d27875adf81b75593670c166bed92847
Caio Labella <caiolabella2016@gmail.com>
2025-01-19 23:41:55 -0300

    feat: adicionar interfaces de cores e temas, implementar função utilitária para mesclar classes

---
+++ src/interfaces/themes.ts
@@ -0,0 +1,62 @@
+export const themes = [
+  {
+    id: "aniversario",
+    name: "Aniversário",
+    image: "/theme-aniversario.svg",
+  },
+  {
+    id: "infantil",
+    name: "Infantil",
+    image: "/theme-infantil.svg",
+  },
+  {
+    id: "formatura",
+    name: "Formatura",
+    image: "/theme-formatura.svg",
+  },
+  {
+    id: "casamento",
+    name: "Casamento",
+    image: "/theme-casamento.svg",
+  },
+  {
+    id: "cha-bebe",
+    name: "Chá de bebê",
+    image: "/theme-cha-bebe.svg",
+  },
+  {
+    id: "cha-panela",
+    name: "Chá de panela",
+    image: "/theme-cha-panela.svg",
+  },
+  {
+    id: "carnaval",
+    name: "Carnaval",
+    image: "/theme-carnaval.svg",
+  },
+  {
+    id: "pascoa",
+    name: "Páscoa",
+    image: "/theme-pascoa.svg",
+  },
+  {
+    id: "sao-joao",
+    name: "São João",
+    image: "/theme-sao-joao.svg",
+  },
+  {
+    id: "halloween",
+    name: "Halloween",
+    image: "/theme-halloween.svg",
+  },
+  {
+    id: "natal",
+    name: "Natal",
+    image: "/theme-natal.svg",
+  },
+  {
+    id: "outro",
+    name: "Outro",
+    image: "/theme-outro.svg",
+  },
+];
