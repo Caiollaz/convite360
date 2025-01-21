53b0a7361ddf97146f9c494e41a64aacb2fd66be
Caio Labella <caiolabella2016@gmail.com>
2025-01-21 13:30:00 -0300

    feat: adicionar configuração do Next.js com suporte a modo estrito e minificação SWC

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true, // Habilita o minificador SWC para produção
+};
+
+module.exports = nextConfig;
