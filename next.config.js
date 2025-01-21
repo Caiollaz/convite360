9f88b558817151b037932362992696f1dc3e85a7
Caio Labella <caiolabella2016@gmail.com>
2025-01-21 13:49:30 -0300

    feat: remover configuração do Next.js e atualizar dependências no package.json e package-lock.json

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true, // Habilita o minificador SWC para produção
-};
-
-module.exports = nextConfig;
